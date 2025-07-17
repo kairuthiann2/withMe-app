d1822e96870f79536c87c8481ecb3001220c83e2
kairuthiann2 <kairuthiann2@gmail.com>
2025-07-17 14:46:23 +0300

    Added toggle functionality for hamburger menu

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.getElementById('navbar');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navbar.classList.toggle('show');
+    });
+  });
+
